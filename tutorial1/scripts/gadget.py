1c546dfc9e21ba4a8302c59d9a6ee8f88fea2b7f
montmasj <montmasj@ROUCWL5317>
2024-04-16 17:37:41 +0200

    port gadget script to gcc

--- tutorial1/scripts/gadget.py
+++ tutorial1/scripts/gadget.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser(prog='gadget.py', description='find ROP gadgets in disassembly file')
 parser.add_argument('strings', nargs='*', type=str, help='strings to search')
-parser.add_argument('-filename', '-f', default='..\Debug\List\disass.txt', help='disassembly file')
+parser.add_argument('-filename', '-f', default='wsf/tutorial1/app/app.txt', help='disassembly file')
 parser.add_argument('-display', '-d', type=int, default=1, help='number of instructions to display')
 
 args = parser.parse_args()
@@ -12,13 +12,15 @@ gadgets = []
 
 with open(args.filename, 'rt') as f:
     for line in f.readlines():
-        if line.startswith('  0x'):
+        if line.startswith('  0x'):  
+            lines.append(line)
+        if line.startswith(' '): # gcc
             lines.append(line)
 
 
 for i in range(len(lines)):
     line = lines[i]
-    if 'POP' in line and 'PC' in line:
+    if 'POP' in line.upper() and 'PC' in line.upper():
         gadgets.append(''.join(lines[i-args.display+d+1] for d in range(args.display)))
 
 for g in gadgets:
